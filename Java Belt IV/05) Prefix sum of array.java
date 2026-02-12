97de8fb545d9bae946bf167c34c642efdeaa6ae5
abhinavravishankar112 <abhinavravi.shankar.s.112@kalvium.community>
2026-02-12 11:26:23 +0530

    Refactor class and update prefix sum logic
    Refactor class name from 'Solution' to 'Main' and modify prefix sum calculation.

--- Java Belt IV/05) Prefix sum of array.java
+++ Java Belt IV/05) Prefix sum of array.java
@@ -4,24 +4,24 @@
 
 import java.util.*;
 
-public class Solution {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
         int[] arr = new int[n];
 
-        // Input
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
 
-        // First element remains same
-        System.out.print(arr[0] + " ");
-
-        // Calculate prefix sum
+        // Calculate prefix sum in place
         for (int i = 1; i < n; i++) {
             arr[i] = arr[i] + arr[i - 1];
+        }
+
+        // Print the prefix sum array
+        for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
     }
